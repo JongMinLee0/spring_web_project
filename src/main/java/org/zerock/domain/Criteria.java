7f947849acd2a277a7c40fb9e1d5f98cc26bd422
JongminLee <jongminlee0218@gmail.com>
2019-08-28 00:47:30 +0900

    Add Paginate

---
+++ src/main/java/org/zerock/domain/Criteria.java
@@ -0,0 +1,51 @@
+package org.zerock.domain;
+
+// 페이징 처리를 위한 클래스
+public class Criteria {
+	private int page;
+	private int perPageNum;
+
+	public Criteria() {
+		this.page = 1;
+		this.perPageNum = 10;
+	}
+
+	public void setPage(int page) {
+		if (page <= 0) {
+			this.page = 1;
+			return;
+		}
+		this.page = page;
+	}
+
+	public void setPerPageNum(int perPageNum) {
+		if (perPageNum <= 0 || perPageNum > 100) {
+			this.perPageNum = 10;
+			return;
+		}
+		this.perPageNum = perPageNum;
+	}
+
+	public int getPage() {
+		return page;
+	}
+
+	// method for MyBatis SQL Mapper
+	public int getPageStart() {
+		return (this.page - 1) * perPageNum + 1;
+	}
+
+	// method for MyBatis SQL Mapper
+	public int getPerPageNum() {
+		return this.perPageNum;
+	}
+	
+	public int getPageEnd() {
+		return (this.getPageStart()+9);
+	}
+
+	@Override
+	public String toString() {
+		return "Criteria [page=" + page + ", perPageNum=" + perPageNum + "]";
+	}
+}
